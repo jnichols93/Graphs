7bd323372710b3d2936494dcb2fac17675db0558
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-03-16 13:05:05 -0500

    more notes

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -93,19 +93,17 @@ class Graph:
                 for neighbor in self.get_neighbors(v):
                     s.push(neighbor)
 
-    def dft_recursive(self, starting_vertex, visited = None):
+    def dft_recursive(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        # check if node visited
-        # if not...
-            # mark as visited
-            # call dft_recursive on each
-        pass 
-
+        # Check if the node has been visited
+        # If not...
+            # Mark it as visited
+            # Call dft_recursive on each neighbor
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -113,7 +111,20 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        # Create a queue
+        # Enqueue A PATH TO the starting vertex
+        # Create a set to store visited vertices
+        # While the queue is not empty...
+            # Dequeue the first PATH
+            # GRAB THE VERTEX FROM THE END OF THE PATH
+            # Check if it's been visited
+            # If it hasn't been visited...
+                # Mark it as visited
+                # CHECK IF IT'S THE TARGET
+                    # IF SO, RETURN THE PATH
+                # Enqueue A PATH TO all it's neighbors
+                    # MAKE A COPY OF THE PATH
+
 
     def dfs(self, starting_vertex, destination_vertex):
         """
