bdb89431d90bc88391f7e5d2432d0160d7e9783a
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-03-17 14:16:46 -0500

    pseudo-code

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,28 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    #This will be a DFS, as we should only need to really search up and down
+    #We will need to create the graph fist
+
+    #Next, go through the ancestors
+
+    #add the vertices for each child and parent
+
+    #  connect children and parents in reverse
+
+    # initialize the queue
+
+    # pay attention to number of ancestors
+
+    #store the earliest ancestor
+
+    # while the queue has items in it
+
+        # pop one off the end and move to the next
+
+        # path is longer or equal and value is smaller or if path is longer
+
+        # set earliest ancestor equal to the vertex and set longest path to the length of path
+
+    # add neighbor to path in back of queue
+    
+    return earliest_ancestor
