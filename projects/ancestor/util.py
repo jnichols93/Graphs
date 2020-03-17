1b71c1b6ea1816d8e24490dc000bacc5a47bf621
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-03-17 14:19:50 -0500

    graph in util file

--- projects/ancestor/util.py
+++ projects/ancestor/util.py
@@ -11,3 +11,35 @@ class Stack():
             return None
     def size(self):
         return len(self.stack)
+
+class Graph:
+    
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        """
+        Add a vertex to the graph.
+        """
+        # TODO
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError('No such vertex')
+
+
+    def get_neighbors(self, vertex_id):
+        """
+        Get all neighbors (edges) of a vertex.
+        """
+        # TODO
+        return self.vertices[vertex_id]
